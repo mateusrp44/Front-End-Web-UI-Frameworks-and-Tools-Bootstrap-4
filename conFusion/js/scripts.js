2cb8462d66cc9e7290aad80d3e2d7888280f7fed
mateusrp44 <mateusrpinto44@gmail.com>
2019-05-16 15:05:22 -0300

    NPM Scripts Part 2

--- conFusion/js/scripts.js
+++ conFusion/js/scripts.js
@@ -1,30 +1,22 @@
 $(document).ready(function(){
-    $("#mycarousel").carousel({ interval: 2000 });
-    $("#carousel-button").click(function() {
-        if($("#carousel-button").children("span").hasClass('fa-pause')) {
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-button").click(function(){
+        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
             $("#mycarousel").carousel('pause');
             $("#carousel-button").children("span").removeClass('fa-pause');
             $("#carousel-button").children("span").addClass('fa-play');
         }
-        else if($("#carousel-button").children("span").hasClass('fa-play')) {
-             $("#mycarousel").carousel('cycle');
-             $("#carousel-button").children("span").removeClass('fa-play');
-             $("#carousel-button").children("span").addClass('fa-pause');
+        else if ($("#carousel-button").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carousel-button").children("span").removeClass('fa-play');
+            $("#carousel-button").children("span").addClass('fa-pause');                    
         }
     });
-    $("#loginButton").Click(function() {
+    $("#loginButton").click(function(){
         $("#loginModal").modal('show');
     });
-    $("#reserveButton").Click(function(){
+    
+    $("#reserveButton").click(function(){
         $("#reserveModal").modal('show');
     });
-})
-/*$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-pause").click(function(){
-        $("#mycarousel").carousel('pause');
-    });
-    $("#carousel-play").click(function(){
-        $("#mycarousel").carousel('cycle');
-    });
-});*/
+});
