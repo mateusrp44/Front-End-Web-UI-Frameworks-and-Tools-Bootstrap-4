4cf2c48ba0a767afc70ae1c3a98626c7658edd6c
mateusrp44 <mateusrpinto44@gmail.com>
2019-05-16 09:12:51 -0300

    NPM Scripts Part 1

---
+++ conFusion/js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carousel-button").click(function() {
+        if($("#carousel-button").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carousel-button").children("span").removeClass('fa-pause');
+            $("#carousel-button").children("span").addClass('fa-play');
+        }
+        else if($("#carousel-button").children("span").hasClass('fa-play')) {
+             $("#mycarousel").carousel('cycle');
+             $("#carousel-button").children("span").removeClass('fa-play');
+             $("#carousel-button").children("span").addClass('fa-pause');
+        }
+    });
+    $("#loginButton").Click(function() {
+        $("#loginModal").modal('show');
+    });
+    $("#reserveButton").Click(function(){
+        $("#reserveModal").modal('show');
+    });
+})
+/*$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carousel-pause").click(function(){
+        $("#mycarousel").carousel('pause');
+    });
+    $("#carousel-play").click(function(){
+        $("#mycarousel").carousel('cycle');
+    });
+});*/
